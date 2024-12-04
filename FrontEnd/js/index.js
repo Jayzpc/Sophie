95bda2f1ea9580fffb84c8caa18811449a260a08
Dunni Olowo <103437195+oladunni6@users.noreply.github.com>
2024-12-03 18:10:22 -0600

    added js_file

---
+++ FrontEnd/js/index.js
@@ -0,0 +1,11 @@
+// fetch call to get jobs
+
+// inside of the second .then function 
+     // create elements and add them to the gallery <div class="gallery">
+
+// Your code should look pretty much the same as the code in index.html after you create it
+
+// add attributes called data-category-<id> to each figure
+// use event listeners to update the class that is on the appropriate gallery item to hide or display them
+
+console.log('Hello World')
