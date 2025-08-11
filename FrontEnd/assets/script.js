b794404225856e1365adeb26229e9aab29d2503a
Jason Black <162649888+Jayzpc@users.noreply.github.com>
2025-08-11 06:41:29 -0400

    commit

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -8,11 +8,7 @@ You set the image source and caption text.
 You append the img and figcaption to the figure, and the figure to the gallery.
 
 
-// .then((response => { /* This line is commented out, but it seems like it was intended to handle the response */
-// if (response.ok) { /* Check if the response status is OK (status code 200) */
-//     return response.json(); /* Parse the response body as JSON 
-// }
-// })) /* This line is commented out, but it seems like it was intended to handle the response */
+handle the response */
 // to see what the response looks like
 
 
@@ -70,14 +66,40 @@ fetch('http://localhost:5678/api/works')
    }
 })
 
-    const myButton = document.getElementById('filter');
-    myButton.addEventListener('click', function() {
-        // Code to execute when the button is clicked
 
 
+     
+        fetch('http://localhost:5678/api/categories')
+.then(response => {
+    if (response.ok) {
+    return response.json()
+    }
+})
 
-        console.log('Button clicked!');
-    });
+    //    console.log('Button clicked!');
+
+.then(data => {
+   const all = document.getElementById('all');
+   // index =0 is all, index 2 is Hotels and restaurants
+
+   for (let index = 2; index < data.length; index++) {
+      const categoryId = data[index];
+      console.log(categoryId); // log each item, not the whole data array
+
+      const name = document.createElement('name');
+      const img = document.createElement('img');
+      const figcaption = document.createElement('figcaption');
+
+   //   img.setAttribute('src', categoryId.imageUrl);
+   //   figcaption.textContent = categoryId.title;
+
+     // figure.appendChild(img);
+    //  figure.appendChild(figcaption);
+    //  all.appendChild(figure);
+   }
+})
+  //  const myButton = document.getElementById('objects');
+  //  all.addEventListener('click', function() {
 
 // gallery.appendChild(figcaption)
 //img
@@ -88,3 +110,23 @@ fetch('http://localhost:5678/api/works')
 
 //postRequest.send();
 
+//const filterButtons = document.querySelector('filterButtons');
+/*const all = document.getElementById('all');
+const objects = document.getElementById('objects');
+const apartments = document.getElementById('apartments');
+const hotels = document.getElementById('hotels');
+
+all.addEventListener('click', () => {
+    console.log();
+}); 
+
+/*objects.addEventListener('click', () => {
+    console.log('Objects button clicked');
+});
+
+apartments.addEventListener('click', () => {
+    console.log('Apartments button clicked');
+});
+
+hotels.addEventListener('click', () => {
+    console.log('Hotels button clicked'); */
