670698da89c8b7be2df1382c79a0c198ef7f6909
Jason Black <162649888+Jayzpc@users.noreply.github.com>
2025-10-13 05:08:28 -0400

    modal Vx

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,1 +1,20 @@
-/* import index from './index.js'; */
+ function displayWorks(works) {
+        const gallery = document.querySelector('modal-body');
+        // Clear the gallery before displaying new or filterworks
+        gallery.innerHTML = '';
+
+        works.forEach(item => {
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            const figcaption = document.createElement('figcaption');
+
+            img.src = item.imageUrl;
+            figcaption.textContent = item.title;
+
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+        });
+
+        displayWorks(allWorks);
+    }
